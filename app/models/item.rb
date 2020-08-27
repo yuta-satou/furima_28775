7732e99607deb31747e20b60f74e60b003b64a22
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-08-28 08:41:56 +0900

    カテゴリーの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  class Item < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+  
+    #空の投稿を保存できないようにする
+    validates :category, presence: true
+  
+    #ジャンルの選択が「--」の時は保存できないようにする
+    validates :category_id, numericality: { other_than: 1 } 
+  end
+  
 end
