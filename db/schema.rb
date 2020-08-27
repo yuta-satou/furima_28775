de1d198b2466c0cf224352838fea8e9ad2386e14
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-08-28 08:34:25 +0900

    itemsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_095312) do
+ActiveRecord::Schema.define(version: 2020_08_27_232019) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "image", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "delivery_change_burden_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "days_up_to_delivery_id", null: false
+    t.integer "price_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_08_19_095312) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
