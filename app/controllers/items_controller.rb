4d13ac79ecd8559c7afad859a961475f5a9c8712
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-08-28 19:35:59 +0900

    itemテーブルに保存実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
     if user_signed_in?
-
+      @item = Item.new
     else
       redirect_to new_user_session_path
     end
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id,
+                                :delivery_change_burden_id, :prefecture_id, :days_up_to_delivery_id, :price_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,29 @@
 class Item < ApplicationRecord
   class Item < ApplicationRecord
+    belongs_to :user
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :category
-  
     #空の投稿を保存できないようにする
     validates :category, presence: true
-  
     #ジャンルの選択が「--」の時は保存できないようにする
     validates :category_id, numericality: { other_than: 1 } 
+    belongs_to_active_hash :status
+    #空の投稿を保存できないようにする
+    validates :status, presence: true
+    #ジャンルの選択が「--」の時は保存できないようにする
+    validates :status_id, numericality: { other_than: 1 }
+
+    belongs_to_active_hash :delivery_change_burden
+    validates :delivery_change_burden, presence: true
+    validates :delivery_change_burden_id, numericality: { other_than: 1 } 
+
+    belongs_to_active_hash :prefecture
+    validates :prefecture, presence: true
+    validates :prefecture_id, numericality: { other_than: 1 } 
+
+    belongs_to_active_hash :days_up_to_delivery
+    validates :days_up_to_delivery, presence: true
+    validates :days_up_to_delivery_id, numericality: { other_than: 1 } 
   end
   
 end
