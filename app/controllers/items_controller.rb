e1d87f94be90a1d4e197d6bc501c13c91669338f
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-09-05 19:06:45 +0900

    商品出品機能修正1-2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    # Item.create(item_params)
     if @item.valid?
       @item.save
       return redirect_to root_path
