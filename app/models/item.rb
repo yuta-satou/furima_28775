0849c028f96f683842c530e6f4f26a23ea207d26
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-09-05 19:24:39 +0900

    商品出品機能修正1-3

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,23 +10,19 @@ class Item < ApplicationRecord
   end
   PRICE_REGEX = /\A[0-9]+\z/
   has_one_attached :image
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true,
-            numericality: { other_than: 1 }
-  validates :status_id, presence: true,
-            numericality: { other_than: 1 }
-  validates :delivery_change_burden_id, presence: true,
-            numericality: { other_than: 1 } 
-  validates :prefecture_id, presence: true,
-            numericality: { other_than: 0 }
-  validates :days_up_to_delivery_id, presence: true,
-            numericality: { other_than: 1 } 
-  validates :price_id, presence: true
-  validates :price_id, numericality: { only_interger: true }  # 数字のみで構成されているか
-  validates :price_id, numericality: { greater_than_or_equal_to: 300 }  # 数字が300以上であるか
-  validates :price_id, numericality: { less_than_or_equal_to: 9999999 }     # 数字が999999以下であるか
-  validates :price_id, format: { with: PRICE_REGEX }
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    with_options numericality: { other_than: 0 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_change_burden_id
+      validates :prefecture_id
+      validates :days_up_to_delivery_id 
+    end
+    validates :price_id, numericality: { only_interger: true }, numericality: { greater_than_or_equal_to: 300 },
+    numericality: { less_than_or_equal_to: 9999999 },format: { with: PRICE_REGEX }
+  end
   
 end
