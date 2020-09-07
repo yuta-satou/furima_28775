152b4298fa15d061bda33c584ce6b2effe0f30bf
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-09-07 11:49:06 +0900

    ルーティング実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,16 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+
+  end
+
+
+
   def show
     @item = Item.find(params[:id])
   end
