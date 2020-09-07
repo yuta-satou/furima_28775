42a20897cb4ea7853bde00a4a8e6329393c4193c
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-09-07 16:54:31 +0900

    商品削除機能修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    return redirect_to root_path
+    if @item.destroy
+      return redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
