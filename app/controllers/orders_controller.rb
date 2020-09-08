830497ce2bb75d6426ee27a48fe33a03821a8e54
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-09-08 18:38:55 +0900

    遷移条件の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,14 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @item = Item.find(params[:item_id])
+    if user_signed_in?
+      @item = Item.find(params[:item_id])
+      if current_user.id == @item.user_id
+        redirect_to root_path
+      end
+    else
+      redirect_to new_user_session_path
+    end
   end
+
 end
