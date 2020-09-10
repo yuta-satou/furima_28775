7b6122c1577b89bccb963f62c470e5623f0b579a
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-09-11 08:13:29 +0900

    その他細かい修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
   resources :items do
-    resources :orders
+    resources :orders, only: [:new, :create]
   end
 end
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -3,9 +3,9 @@ FactoryBot.define do
     token {"sdfadfasjdkfk"}
     postal_code {"000-4563"}
     prefecture_id {2} 
-    city {"asdf"} 
-    block {"asdf"}
-    building_name {"adsfasd"}
+    city {"横浜"} 
+    block {"青山"}
+    building_name {"柳ビル"}
     phone_number {"09011110000"}
   end
 end
