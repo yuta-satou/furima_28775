5610d776e38b0a6a368f628771ef0250ad78b983
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-09-11 14:55:50 +0900

    商品購入機能修正1-4

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -9,32 +9,32 @@ RSpec.describe OrderAddress, type: :model do
     it '全ての項目が存在すれば登録できること' do
       expect(@order).to be_valid
     end
-    it 'クレジットカード情報が存在すれば登録できること' do
+    it 'クレジットカード情報が存在しないと登録できないこと' do
       @order.token = nil
       @order.valid?
       expect(@order.errors.full_messages).to include("Token can't be blank")
     end
-    it '郵便番号情報が存在すれば登録できること' do
+    it '郵便番号情報が存在しないと登録できないこと' do
       @order.postal_code = nil
       @order.valid?
       expect(@order.errors.full_messages).to include("Postal code can't be blank")
     end
-    it '郵便番号情報に"-"が存在すれば登録できること' do
+    it '郵便番号情報に"-"が存在しないと登録できないこと' do
       @order.postal_code = "0009999"
       @order.valid?
       expect(@order.errors.full_messages).to include("Postal code is invalid")
     end
-    it '都道府県情報が存在すれば登録できること' do
+    it '都道府県情報が存在しないと登録できないこと' do
       @order.prefecture_id = 0
       @order.valid?
       expect(@order.errors.full_messages).to include("Prefecture must be other than 0")
     end
-    it '市区町村情報が存在すれば登録できること' do
+    it '市区町村情報が存在しないと登録できないこと' do
       @order.city = nil
       @order.valid?
       expect(@order.errors.full_messages).to include("City can't be blank")
     end
-    it '番地情報が存在すれば登録できること' do
+    it '番地情報が存在しないと登録できないこと' do
       @order.block = nil
       @order.valid?
       expect(@order.errors.full_messages).to include("Block can't be blank")
@@ -43,12 +43,12 @@ RSpec.describe OrderAddress, type: :model do
       @order.building_name = nil
       @order.valid?
     end
-    it '電話番号情報が存在すれば登録できること' do
+    it '電話番号情報が存在しないと登録できないこと' do
       @order.phone_number = nil
       @order.valid?
       expect(@order.errors.full_messages).to include("Phone number can't be blank")
     end
-    it '電話番号情報に"-"が存在しなければ登録できること' do
+    it '電話番号情報に"-"が存在するとと登録できないこと' do
       @order.phone_number = "09000001112"
       @order.valid?
       expect(@order.errors.full_messages).to include("Phone number is invalid")
