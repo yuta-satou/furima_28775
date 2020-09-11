acc34b428e30471fe15d7522c635d5e026890eb2
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-09-11 15:31:14 +0900

    商品購入機能修正3

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -5,53 +5,57 @@ RSpec.describe OrderAddress, type: :model do
     before do
       @order = FactoryBot.build(:order_address)
     end
+    context '新規登録ができるとき' do
+      it '全ての項目が存在すれば登録できること' do
+        expect(@order).to be_valid
+      end
+      it '建物名が存在しなくても登録できること' do
+        @order.building_name = nil
+        @order.valid?
+      end
+    end
 
-    it '全ての項目が存在すれば登録できること' do
-      expect(@order).to be_valid
-    end
-    it 'クレジットカード情報が存在しないと登録できないこと' do
-      @order.token = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Token can't be blank")
-    end
-    it '郵便番号情報が存在しないと登録できないこと' do
-      @order.postal_code = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Postal code can't be blank")
-    end
-    it '郵便番号情報に"-"が存在しないと登録できないこと' do
-      @order.postal_code = "0009999"
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Postal code is invalid")
-    end
-    it '都道府県情報が存在しないと登録できないこと' do
-      @order.prefecture_id = 0
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Prefecture must be other than 0")
-    end
-    it '市区町村情報が存在しないと登録できないこと' do
-      @order.city = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("City can't be blank")
-    end
-    it '番地情報が存在しないと登録できないこと' do
-      @order.block = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Block can't be blank")
-    end
-    it '建物名が存在しなくても登録できること' do
-      @order.building_name = nil
-      @order.valid?
-    end
-    it '電話番号情報が存在しないと登録できないこと' do
-      @order.phone_number = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Phone number can't be blank")
-    end
-    it '電話番号情報に"-"が存在するとと登録できないこと' do
-      @order.phone_number = "09000001112"
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Phone number is invalid")
+    context '新規登録ができないとき' do
+      it 'クレジットカード情報が存在しないと登録できないこと' do
+        @order.token = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Token can't be blank")
+      end
+      it '郵便番号情報が存在しないと登録できないこと' do
+        @order.postal_code = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号情報に"-"が存在しないと登録できないこと' do
+        @order.postal_code = "0009999"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '都道府県情報が存在しないと登録できないこと' do
+        @order.prefecture_id = 0
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Prefecture must be other than 0")
+      end
+      it '市区町村情報が存在しないと登録できないこと' do
+        @order.city = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地情報が存在しないと登録できないこと' do
+        @order.block = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Block can't be blank")
+      end
+      it '電話番号情報が存在しないと登録できないこと' do
+        @order.phone_number = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号情報に"-"が存在すると登録できないこと' do
+        @order.phone_number = "090-000-01112"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number is invalid")
+      end
     end
   end
 end
